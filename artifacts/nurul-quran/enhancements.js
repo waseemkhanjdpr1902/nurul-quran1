17102a022e1ac149f654bd12d1d680d2b3d7e986
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-04-30 13:09:40 +0530

    Update enhancements.js

--- artifacts/nurul-quran/enhancements.js
+++ artifacts/nurul-quran/enhancements.js
@@ -1,3 +1,30 @@
+async function fetchSalahTimes() {
+    const display = document.getElementById('salah-display');
+    
+    const showTimes = async (lat, lng, label = "") => {
+        try {
+            const res = await fetch(`https://api.aladhan.com/v1/timings?latitude=${lat}&longitude=${lng}&method=2`);
+            const data = await res.json();
+            const t = data.data.timings;
+            display.innerHTML = `
+                ${label} <b>Fajr:</b> ${t.Fajr} | <b>Dhuhr:</b> ${t.Dhuhr} | <b>Asr:</b> ${t.Asr} | 
+                <b>Maghrib:</b> ${t.Maghrib} | <b>Isha:</b> ${t.Isha}
+            `;
+        } catch (err) {
+            display.innerText = "Connection Error";
+        }
+    };
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (pos) => showTimes(pos.coords.latitude, pos.coords.longitude),
+            () => showTimes(21.4225, 39.8262, "(Makkah) ") // Default to Makkah if GPS denied
+        );
+    } else {
+        showTimes(21.4225, 39.8262, "(Makkah) ");
+    }
+}
+fetchSalahTimes();
 // --- 1. Salah Times Logic ---
 async function fetchSalahTimes() {
     const display = document.getElementById('salah-display');
