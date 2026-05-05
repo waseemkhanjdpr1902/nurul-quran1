b3e444945bc22cfda644eea4f1aa21ba264fe795
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-05-05 22:35:56 +0530

    Update main.tsx

--- artifacts/nurul-quran/src/main.tsx
+++ artifacts/nurul-quran/src/main.tsx
@@ -1,8 +1,26 @@
-import { createRoot } from "react-dom/client";
-import { setAuthTokenGetter } from "@workspace/api-client-react";
-import App from "./App";
-import "./index.css";
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-setAuthTokenGetter(() => localStorage.getItem("nurulquran_token"));
+import Home from "./pages/Home";
+import Quran from "./pages/Quran";
+import Discover from "./pages/Discover";
+import Library from "./pages/Library";
+import Courses from "./pages/Courses";
+import NotFound from "./pages/NotFound";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/quran", element: <Quran /> },
+  { path: "/discover", element: <Discover /> },
+  { path: "/library", element: <Library /> },
+  { path: "/courses", element: <Courses /> },
+  { path: "*", element: <NotFound /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
